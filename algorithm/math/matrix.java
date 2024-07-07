f4d5df8acd100330dcd54f7d78fda7b5bd2e8064
luyong <2630914115@qq.com>
2024-07-07 10:07:17 +0800

    math add matrix class

---
+++ algorithm/math/matrix.java
@@ -0,0 +1,21 @@
+package algorithm.math;
+
+public class matrix {
+    
+    // 矩阵乘法
+    static long[][] times(long[][] mat1, long[][] mat2) {
+        if (mat1[0].length != mat2.length) {
+            return null;
+        }
+        int n = mat1.length, m = mat2[0].length;
+        long[][] ans = new long[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < mat2.length; k++) {
+                    ans[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
