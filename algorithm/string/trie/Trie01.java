e50f853c0bef64d1f0d29a14ff4bd7d7fba5b556
luyong <2630914115@qq.com>
2024-08-21 08:27:25 +0800

    update trie 0-1

--- algorithm/string/trie/Trie01.java
+++ algorithm/string/trie/Trie01.java
@@ -2,81 +2,56 @@ package algorithm.string.trie;
 
 /**
  * 0-1 trie
- * 测试链接：https://leetcode.cn/problems/ms70jA/
  */
 public class Trie01 {
     
-    static int MAXN = 200001 * 31;
-    static int[][] trie = new int[MAXN][2];
-    static int[] pass = new int[MAXN], end = new int[MAXN];
-    static int no;
+    static class Trie {
+        private Node root = new Node();
 
-    static void insert(int v) {
-        int cur = 1;
-        for (int i = 30; i >= 0; i--) {
-            int j = v >> i & 1;
-            if (trie[cur][j] == 0) {
-                trie[cur][j] = ++no;
-            }
-            cur = trie[cur][j];
-            pass[cur]++;
+        private class Node {
+            int cnt;
+            Node[] nxt = new Node[2];
         }
-        end[cur]++;
-    }
 
-    // v 一定在字典树中
-    static void delete(int v) {
-        int cur = 1;
-        for (int i = 30; i >= 0; i--) {
-            int j = v >> i & 1;
-            if (--pass[trie[cur][j]] == 0) {
-                trie[cur][j] = 0;
-                return;
-            }
-            cur = trie[cur][j];
-        }
-        end[cur]--;
-    }
-
-    static boolean search(int v) {
-        int cur = 1;
-        for (int i = 30; i >= 0; i--) {
-            cur = trie[cur][v >> i & 1];
-            if (cur == 0) {
-                return false;
-            }
-        }
-        return end[cur] > 0;
-    }
-
-    static void clear() {
-        for (int i = 1; i <= no; i++) {
-            trie[i][0] = trie[i][1] = pass[i] = end[i] = 0;
-        }
-        no = 1;
-    }
-
-    public int findMaximumXOR(int[] nums) {
-        clear();
-        for (int v : nums) {
-            insert(v);
-        }
-        int ans = 0;
-        for (int v : nums) {
-            int cur = 1;
-            int best = 0;
+        private void insert(int v) {
+            Node cur = root;
             for (int i = 30; i >= 0; i--) {
                 int j = v >> i & 1;
-                if (trie[cur][j ^ 1] != 0) {
-                    best |= (j ^ 1) << i;
-                    cur = trie[cur][j ^ 1];
+                if (cur.nxt[j] == null) {
+                    cur.nxt[j] = new Node();
+                }
+                cur = cur.nxt[j];
+                cur.cnt++;
+            }
+        }
+
+        private void delete(int v) {
+            Node cur = root;
+            for (int i = 30; i >= 0; i--) {
+                int j = v >> i & 1;
+                if (cur.nxt[j].cnt == 1) {
+                    cur.nxt[j] = null;
+                    return;
+                }
+                cur = cur.nxt[j];
+                cur.cnt--;
+            }
+        }
+
+        private int maxXor(int v) {
+            Node cur = root;
+            int ans = 0;
+            for (int i = 30; i >= 0; i--) {
+                int j = v >> i & 1, k = j ^ 1;
+                if (cur.nxt[k] != null) {
+                    ans |= 1 << i;
+                    cur = cur.nxt[k];
                 } else {
-                    best |= j << i;
-                    cur = trie[cur][j];
+                    cur = cur.nxt[j];
                 }
             }
-            ans = Math.max(ans, v ^ best);
+            return ans;
         }
-        return ans;
+
     }
 }
