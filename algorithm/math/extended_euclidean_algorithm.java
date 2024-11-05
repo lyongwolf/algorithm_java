d746b617a29cbf3224f63ab6aafbe823b72b913e
luyong <2630914115@qq.com>
2024-11-06 02:37:17 +0800

    exgcd

--- algorithm/math/extended_euclidean_algorithm.java
+++ algorithm/math/extended_euclidean_algorithm.java
@@ -6,27 +6,12 @@ package algorithm.math;
 
 public class extended_euclidean_algorithm {
     
-    public static boolean linearEquation(long[] ans, long a, long b, long m) {
-        long d = ext_gcd(ans, a, b);
-        if (m % d != 0) {
-            return false;
-        }
-        long n = m / d;
-        ans[0] *= n;
-        ans[1] *= n;
-        return true;
-    }
-
-    public static long ext_gcd(long[] ans, long a, long b) {
+    static int[] exgcd(int a, int b) {
         if (b == 0) {
-            ans[0] = 1;
-            ans[1] = 0;
-            return a;
+            return new int[]{1, 0};
         }
-        long res = ext_gcd(ans, b, a % b);
-        long tmp = ans[0];
-        ans[0] = ans[1];
-        ans[1] = tmp - a / b * ans[1];
-        return res;
+        int[] ans = exgcd(b, a % b);
+        ans[1] = ans[0] - a / b * (ans[0] = ans[1]);
+        return ans;
     }
 }
