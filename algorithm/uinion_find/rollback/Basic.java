4ca24e659c8c0c42d0ff855f9f3357be76a7db29
luyong <2630914115@qq.com>
2025-04-15 14:59:02 +0800

    .

--- algorithm/uinion_find/rollback/Basic.java
+++ algorithm/uinion_find/rollback/Basic.java
@@ -4,9 +4,53 @@ import java.util.*;
 
 /**
  * 可撤销并查集（模板）
- * 测试链接：
  */
 
-public class Basic {
+ class DSURollback {
+    private static final int MAXN = 500000;
+    private static int[] stkF0 = new int[MAXN], stkF1 = new int[MAXN];
+    private int op;
+    
+    int[] parent, sz;
+
+    public DSURollback(int n) {
+        n++;
+        parent = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int find(int v) {
+        while (parent[v] != v) {
+            v = parent[v];
+        }
+        return v;
+    }
+
+    public void union(int v0, int v1) {
+        int f0 = find(v0), f1 = find(v1);
+        if (sz[f0] < sz[f1]) {
+            f0 ^= f1 ^ (f1 = f0);
+        }
+        stkF0[op] = f0;
+        stkF1[op] = f1;
+        if (f0 != f1) {
+            parent[f1] = f0;
+            sz[f0] += sz[f1];
+        }
+        op++;
+    }
+
+    public void undo() {
+        op--;
+        int f0 = stkF0[op], f1 = stkF1[op];
+        if (f0 != f1) {
+            parent[f1] = f1;
+            sz[f0] -= sz[f1];
+        }
+    }
     
 }
