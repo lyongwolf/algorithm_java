c10322b70be7be8296fbe6735ef1cc442391b110
luyong <2630914115@qq.com>
2025-06-03 17:30:20 +0800

    .

--- algorithm/z_daily/Mat.java
+++ algorithm/z_daily/Mat.java
@@ -2,9 +2,6 @@ public class Mat {
     
     // 矩阵乘法
     long[][] mul(long[][] mat1, long[][] mat2) {
-        if (mat1[0].length != mat2.length) {
-            return null;
-        }
         int n = mat1.length, m = mat2[0].length;
         long[][] ans = new long[n][m];
         for (int i = 0; i < n; i++) {
