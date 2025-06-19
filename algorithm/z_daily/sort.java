59ffa5329e951a4491455b8f7ef6db9d5d8bfcd9
luyong <2630914115@qq.com>
2025-06-19 08:55:22 +0800

    .

--- algorithm/z_daily/sort.java
+++ algorithm/z_daily/sort.java
@@ -23,6 +23,30 @@ class Sort {
         }
     }
 
+    public static void radix(long[] arr) {
+        radix(arr, 0, arr.length - 1);
+    }
+    
+    public static void radix(long[] arr, int l, int r) {
+        if (r - l + 1 <= 1000) {java.util.Arrays.sort(arr, l, r + 1); return;}
+        int len = r - l + 1, b = 4, n = 1 << b, m, mx = 0, x, i, j;
+        while ((n <<= 1) <= len) b++;
+        m = (n >>= 1) - 1;
+        int[] cnt = new int[n];
+        long[] tmp = new long[len];
+        for (i = l; i <= r; i++) cnt[(int) (arr[i] & m)]++;
+        for (i = 1; i < n; i++) cnt[i] = cnt[i - 1] + cnt[i];
+        for (i = r; i >= l; i--) tmp[--cnt[(int) (arr[i] & m)]] = arr[i];
+        for (i = l, j = 0; i <= r; arr[i++] = tmp[j++]) mx = Math.max(mx, 64 - Long.numberOfLeadingZeros(arr[i]));
+        for (x = b; x < mx; x += b) {
+            java.util.Arrays.fill(cnt, 0);
+            for (i = l; i <= r; i++) cnt[(int) (arr[i] >> x & m)]++;
+            for (i = 1; i < n; i++) cnt[i] = cnt[i - 1] + cnt[i];
+            for (i = r; i >= l; i--) tmp[--cnt[(int) (arr[i] >> x & m)]] = arr[i];
+            for (i = l, j = 0; i <= r; i++) arr[i] = tmp[j++];
+        }
+    }
+
     public static void merge(int[] a, java.util.function.IntBinaryOperator cmp) {merge(a.clone(), a, 0, a.length, 0, cmp);}
 
     public static void merge(int[] a, int l, int r, java.util.function.IntBinaryOperator cmp) {merge(java.util.Arrays.copyOfRange(a, l, r), a, l, r, -l, cmp);}
