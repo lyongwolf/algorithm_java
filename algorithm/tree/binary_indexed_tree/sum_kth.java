a1a2b9666a349b46f4556dbab8b438ce1ad7de67
luyong <2630914115@qq.com>
2025-06-24 04:20:46 +0800

    .

--- algorithm/tree/binary_indexed_tree/sum_kth.java
+++ algorithm/tree/binary_indexed_tree/sum_kth.java
@@ -4,14 +4,31 @@ class Fenwick {
     
     public Fenwick(int len) {
         N = len;
-        sum = new int[N + 1];
         hb = Integer.highestOneBit(N);
+        sum = new int[N + 1];
     }
 
     public void add(int o, int v) {
         for (; o <= N; o += o & -o) {
             sum[o] += v;
         }
+    }
+
+    public int pre(int i) {
+        int ans = 0;
+        while (i > 0) {
+            ans += sum[i];
+            i -= i & -i;
+        }
+        return ans;
+    }
+
+    public int suf(int i) {
+        return range(i, N);
+    }
+
+    public int range(int l, int r) {
+        return l > r ? 0 : pre(r) - pre(l - 1);
     }
 
     public int smallK(int k) {
