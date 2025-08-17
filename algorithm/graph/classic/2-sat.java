38367365da6498243f110f3cd46e4b8b8c29db8a
luyong <2630914115@qq.com>
2025-08-17 14:36:11 +0800

    .

--- algorithm/graph/classic/2-sat.java
+++ algorithm/graph/classic/2-sat.java
@@ -3,6 +3,7 @@ class TwoSat {
     // if i ∈ [1, n] is true, then i + n must be false
     // if i ∈ [1, n] is false, then i + n must be true
 
+    private int[] stack;
     private int[] head, nxt, to;
     private int[] dfn, low, stk, scc;
     private boolean[] vis;
@@ -12,6 +13,7 @@ class TwoSat {
         this.n = n;
         z = 1;
         int m = n << 1 | 1;
+        stack = new int[m];
         head = new int[m];
         nxt = new int[m];
         to = new int[m];
@@ -51,25 +53,56 @@ class TwoSat {
     }
 
     private void tarjan(int u) {
-        dfn[u] = low[u] = ++ts;
-        stk[++top] = u;
-        vis[u] = true;
-        for (int e = head[u], v = to[e]; e != 0; e = nxt[e], v = to[e]) {
-            if (dfn[v] == 0) {
-                tarjan(v);
-                low[u] = Math.min(low[u], low[v]);
-            } else if (vis[v]) {
-                low[u] = Math.min(low[u], dfn[v]);
+        int z = 1;
+        stack[1] = u;
+        while (z > 0) {
+            u = stack[z];
+            if (dfn[u] == 0) {
+                dfn[u] = low[u] = ++ts;
+                stk[++top] = u;
+                vis[u] = true;
+                for (int e = head[u], v; e != 0; e = head[u]) {
+                    v = to[e];
+                    if (dfn[v] == 0) {
+                        stack[++z] = v;
+                        break;
+                    }
+                    if (vis[v]) {
+                        low[u] = Math.min(low[u], dfn[v]);
+                    }
+                    head[u] = nxt[e];
+                }
+            } else {
+                int e = head[u];
+                if (e != 0) {
+                    low[u] = Math.min(low[u], low[to[e]]);
+                    head[u] = nxt[e];
+                    e = head[u];
+                    for (int v; e != 0; e = head[u]) {
+                        v = to[e];
+                        if (dfn[v] == 0) {
+                            stack[++z] = v;
+                            break;
+                        }
+                        if (vis[v]) {
+                            low[u] = Math.min(low[u], dfn[v]);
+                        }
+                        head[u] = nxt[e];
+                    }
+                }
+                if (e == 0) {
+                    z--;
+                    if (dfn[u] == low[u]) {
+                        no++;
+                        int o;
+                        do {
+                            o = stk[top--];
+                            vis[o] = false;
+                            scc[o] = no;
+                        } while (o != u);
+                    }
+                }
             }
-        }
-        if (dfn[u] == low[u]) {
-            no++;
-            int o;
-            do {
-                o = stk[top--];
-                vis[o] = false;
-                scc[o] = no;
-            } while (o != u);
         }
     }
 }
